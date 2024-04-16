bc3a870e3adca9c668d5245b82c565d3fd992b68
DevDominik <71924117+DevDominik@users.noreply.github.com>
2024-04-16 16:48:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-let tarolo = document.getElementById("container")
-
-let betoltKepek = async function() {
-    return (await (await fetch("/lista.txt")).text()).trim().split("\n")
-}
-
-let betoltott = await betoltKepek()
-for (let index = 0; index < betoltott.length; index++) {
-    let kartya = document.createElement("div")
-    let kep = document.createElement("img")
-    kartya.classList.add("kartya")
-    kep.classList.add("kep-main")
-    kep.src = `images/${betoltott[index]}`
-    kartya.appendChild(kep)
-    tarolo.appendChild(kartya)
+let betoltKepek = async function() {    
+    (await (await fetch("/lista.txt")).text()).trim().split("\n").forEach(elem => {
+        let kartya = document.createElement("div")
+        let kep = document.createElement("img")
+        kartya.classList.add("kartya")
+        kep.classList.add("kep-main")
+        kep.src = `images/${elem}`
+        kartya.appendChild(kep)
+        document.getElementById("container").appendChild(kartya)
+    })
 }
 
 window.addEventListener("resize", () => {
     let valasztott = "kep1"
-    if (window.matchMedia("min-width: 1280px").matches) {
+    if (window.matchMedia("(min-width: 1280px)").matches) {
         valasztott = "kep3"
-    } else if (window.matchMedia("min-width: 768px").matches) {
+    } else if (window.matchMedia("(min-width: 768px)").matches) {
         valasztott = "kep2"
     }
-    let query = document.querySelectorAll(".kartya img")
+    let query = document.querySelectorAll(".kartya")
     for (let index = 0; index < query.length; index++) {
         query[index].classList.remove("kep1")
         query[index].classList.remove("kep2")
@@ -30,3 +25,5 @@ window.addEventListener("resize", () => {
         query[index].classList.add(valasztott)
     }
 })
+
+betoltKepek()
